ef55ea84243d927c0c22f9c53a8c11fd17db9204
kevit-ashish-chandpa <ashish.chandpa@kevit.io>
2022-06-28 18:59:49 +0530

    Updateddd

--- public/js/app.js
+++ public/js/app.js
@@ -1,45 +1,5 @@
 
-
-
-// const weatherForm = document.querySelector('form')
-// const search = document.querySelector('input')
-// const messageOne = document.querySelector('#message-1')
-// const messageTwo = document.querySelector('#message-2')
-
-// weatherForm.addEventListener("submit", async (e) => {
-//   e.preventDefault(); 
-
-//   const location = search.value;
-
-//   messageOne.textContent = "Loading...";
-//   messageTwo.textContent = "";
-
-//   try {
-//     const response = await axios.get('/weather?address=' + location);
-//     const data = response.data;
-//     if (data.error) {
-//       // console.log(data.error);
-//       messageOne.textContent = data.error;
-//     } else {
-//     //   messageOne.textContent = data.location;
-//     //   messageTwo.textContent = data.forecast;
-//     console.log(data.location);
-//     }z   
-//   } catch (e) {}
-//   console.log(e);
-// //   messageOne.textContent = data.e;
-// });
-
-
-
-
-// import axios from 'axios';
 // const axios = require('axios')
-
-// const { default: axios } = require("axios")
-
-// const axios = require("axios")
-
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -49,7 +9,47 @@ const messageTwo = document.querySelector('#message-2')
 messageOne.textContent = 'From Javascript'
 
 // debugger
+//
+// console.log('Client side JavaScript File')
+// const url = 'http://localhost:3000/weather?address=' + address
+// axios.get(url).then(({ data }) => {
+//     // console.log(data.location);
+//     // console.log(data.forcast);
+//     messageOne.textContent = data.place
+//     messageTwo.textContent = data.forecast
+// }).catch((error) => {
+//     if (error.response) {
+//         messageOne.textContent = data.error;
+//     }
+//     else if (error.request) {
+//         callback('Unable to connect to weather services!', undefined)
+//     }
+//     else {
+//         callback('Cannot search the weather, check for the new latitude and longitude', undefined)
+//     }
 
+// })
+
+//     //1st implementaton
+
+//     .then((response) => {
+//         response.json().then((data) => {
+//             if (data.error) {
+//                 // console.log(data.error)
+//                 messageOne.textContent = data.error;
+//             } else {
+//                 messageOne.textContent = data.place
+//                 messageTwo.textContent = data.forecast
+//             }
+//         })
+//     })
+//     .catch((error) => {
+//         console.log("Hello");
+//     })
+
+//2 nd implementation
+
+//
 
 weatherForm.addEventListener('submit', async (e) => {
     e.preventDefault()
@@ -59,7 +59,7 @@ weatherForm.addEventListener('submit', async (e) => {
     messageOne.textContent = 'Loading!!!!'
     messageTwo.textContent = ''
     //hello git
-    console.log("Itsss workingg");
+    
     try {
         const response = await axios.get('/weather?address=' + location);
         const data = response.data;
@@ -73,38 +73,6 @@ weatherForm.addEventListener('submit', async (e) => {
         }
     } catch (e) { }
     console.log(e);
-    //   messageOne.textContent = data.e;
-
-
-    // axios.get('/weather?address=' + location)
-    //     .then((response) => {
-    //         response.json().then((data) => {
-    //             if (data.error) {
-    //                 // console.log(data.error)
-    //                 messageOne.textContent = data.error;
-    //             } else {
-    //                 messageOne.textContent = data.place
-    //                 messageTwo.textContent = data.forecast
-    //             }
-    //         })
-    //     })
-    //     .catch((error) => {
-    //         console.log("Hello");
-    //     })
-
-    // .then((response) => console.log(response.data));
-
-
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                // console.log(data.error)
-                messageOne.textContent = data.error;
-            } else {
-                messageOne.textContent = data.place
-                messageTwo.textContent = data.forecast
-            }
-        })
-    })
+    console.log("Itsss workingg");
 })
 
