deb1794e08ac03e3b266634e506a5627b388116b
kevit-ashish-chandpa <ashish.chandpa@kevit.io>
2022-06-28 17:21:23 +0530

    Its app.js using axios

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@
 //     //   messageOne.textContent = data.location;
 //     //   messageTwo.textContent = data.forecast;
 //     console.log(data.location);
-//     }
+//     }z   
 //   } catch (e) {}
 //   console.log(e);
 // //   messageOne.textContent = data.e;
@@ -51,35 +51,49 @@ messageOne.textContent = 'From Javascript'
 // debugger
 
 
-weatherForm.addEventListener('submit', (e) => {
+weatherForm.addEventListener('submit', async (e) => {
     e.preventDefault()
-    
+
     const location = search.value
 
     messageOne.textContent = 'Loading!!!!'
     messageTwo.textContent = ''
     //hello git
     console.log("Itsss workingg");
+    try {
+        const response = await axios.get('/weather?address=' + location);
+        const data = response.data;
+        if (data.error) {
+            // console.log(data.error);
+            messageOne.textContent = data.error;
+        } else {
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
+            // console.log(data.location);
+        }
+    } catch (e) { }
+    console.log(e);
+    //   messageOne.textContent = data.e;
 
-    // // axios.get('/weather?address=' + location)
-    // axios.create({baseUrl: '/weather?address=' + location})
+
+    // axios.get('/weather?address=' + location)
     //     .then((response) => {
     //         response.json().then((data) => {
     //             if (data.error) {
     //                 // console.log(data.error)
     //                 messageOne.textContent = data.error;
-    //             } else {                
+    //             } else {
     //                 messageOne.textContent = data.place
     //                 messageTwo.textContent = data.forecast
     //             }
     //         })
     //     })
-    //     .catch((error)=>{
+    //     .catch((error) => {
     //         console.log("Hello");
     //     })
 
     // .then((response) => console.log(response.data));
-           
+
 
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
